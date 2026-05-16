000aee1ae4be925c16ba759e12f9351ecd45a9d1
Camila Pizzani <camilapizzani24@gmail.com>
2026-05-15 23:23:29 -0300

    Update mailerlite-subscribe.js

--- api/mailerlite-subscribe.js
+++ api/mailerlite-subscribe.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': Bearer ${process.env.MAILERLITE_API_KEY}
+        'Authorization': `Bearer ${process.env.MAILERLITE_API_KEY}`
       },
       body: JSON.stringify({ email, status: 'active' })
     });
